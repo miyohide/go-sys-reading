230762cba84ce46aaf448df62ae145660f3c955e
Hidenori Miyoshi <miyohide@gmail.com>
2018-03-05 22:32:06 +0900

    Add P50-51. sample programs

---
+++ 3.6.1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+var source = `1行目
+2行目
+3行目`
+
+func main() {
+	reader := bufio.NewReader(strings.NewReader(source))
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Printf("%#v\n", line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	fmt.Println("==================================")
+
+	scanner := bufio.NewScanner(strings.NewReader(source))
+	for scanner.Scan() {
+		fmt.Printf("%#v\n", scanner.Text())
+	}
+}
